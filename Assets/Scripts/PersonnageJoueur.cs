4f61f5ecce01f03b19807a78b6225f6bb253d6cd
Guillaume <guilllaume.sacre@gmail.com>
2018-07-18 09:09:15 +0200

    Saut personnage

--- Assets/Scripts/PersonnageJoueur.cs
+++ Assets/Scripts/PersonnageJoueur.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 public class PersonnageJoueur : MonoBehaviour 
 {
 	#region Variables (public) 
- 
+
+	public Rigidbody m_pRigidbody = null;
+
 	public  int m_iPV = 10;
   
 	public float m_fVitesse = 2.0f;
+
+	public float m_fVitesseSaut = 5.0f;
+
 	#endregion
 
-#region Variables (private)                       
+	#region Variables (private)                       
 
 
 
@@ -20,7 +25,11 @@ public class PersonnageJoueur : MonoBehaviour
 	private void Update()
 	{
 		BougerPersonnage();
-	}
+
+		Jump();
+
+	}	
+
 	private void BougerPersonnage()
 	{
 		float fAxeHorizontal = Input.GetAxis("Horizontal");
@@ -33,11 +42,19 @@ public class PersonnageJoueur : MonoBehaviour
 			direction.Normalize();
 
 			Vector3 deplacement = direction * (m_fVitesse * Time.deltaTime);
-			transform.position += deplacement;
-
+			m_pRigidbody.MovePosition(transform.position + deplacement);
+			
 			transform.forward = direction;
 		}
 	}
 	
+	private void Jump()
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			Vector3 tSaut = Vector3.up * m_fVitesseSaut;
+			m_pRigidbody.AddForce(tSaut, ForceMode.Impulse);
 
+		}
+	}
 } 
