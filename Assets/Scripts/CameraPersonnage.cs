1cb2317ac86cd814cf5616ca9d7541a53704e3af
Guillaume <guilllaume.sacre@gmail.com>
2018-07-18 09:08:57 +0200

    Camera follow personnage

---
+++ Assets/Scripts/CameraPersonnage.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+ 
+public class CameraPersonnage : MonoBehaviour
+{ 
+	#region Variables (public)
+
+	public PersonnageJoueur m_pTarget = null;
+
+	public float m_fDistanceDeSuivi = 0.0f;
+
+	#endregion
+ 
+	#region Variables (private)
+
+
+	#endregion
+
+	private void LateUpdate()
+	{
+		if (m_pTarget !=null)
+		Follow();
+	}
+
+	private void Follow()
+	{
+		Vector3 nouvellePosition = (m_pTarget.transform.position) - (transform.forward * m_fDistanceDeSuivi);
+		transform.position = nouvellePosition;
+	}
+}
