702b38f2178554023dd95e2662bfc292c249ab0f
Guillaume <guilllaume.sacre@gmail.com>
2018-07-19 15:15:37 +0200

    CreationMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Variables (public)
+
+	public string m_sNomDeLaScene = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+	public void LancerLeJeu()
+	{
+		SceneManager.LoadScene(m_sNomDeLaScene);
+	}
+	public void QuitterLeJeu()
+	{
+		Application.Quit();
+	}
+}
